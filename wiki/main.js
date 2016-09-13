1d78777b79f383a3933a0b2cbad3b6cb98b2455e
gideonamani <gmarress@gmail.com>
2016-09-13 20:04:44 +0300

    Add a countdown timer

--- wiki/main.js
+++ wiki/main.js
@@ -126,6 +126,24 @@ $(document).ready(function(){
 
   });
 
+// The flip clock plugin for an awesome countdown timer.
+      var clock;
+
+      clock = $('.clock').FlipClock({
+            clockFace: 'DailyCounter',
+            autoStart: false,
+            callbacks: {
+              stop: function() {
+                $('.message').html('The clock has stopped!')
+              }
+            }
+        });
+            
+        clock.setTime(220880);
+        clock.setCountdown(true);
+        clock.start();
+
 
 });
+
 
