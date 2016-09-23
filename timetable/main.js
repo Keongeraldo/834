9c7e780c1a0d9bd5d121b17decc4c31eff5111d5
gideonamani <gmarress@gmail.com>
2016-09-24 01:32:42 +0300

    make minor changes

--- timetable/main.js
+++ timetable/main.js
@@ -151,7 +151,7 @@ $(document).ready(function(){
           todayHighlight: true,
           weekStart: 1,
           daysOfWeekHighlighted: "0,6",
-          calendarWeeks: true
+          // calendarWeeks: true
 
       });
 
